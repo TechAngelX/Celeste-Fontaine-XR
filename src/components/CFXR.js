1e59ecbd735972745a5f0d0e55c92a4910a1043e
Ricki Angel <angelricki@outlook.com>
2024-10-15 22:42:25 +0100

    Iterate.

---
+++ src/components/CFXR.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/CFXR.css';
+
+const CFXR = () => {
+    return (
+        <div className="celeste-fontaine">
+            <img src="./images/logo.png" alt="Logo" className="celeste-fontaine-logo" />
+        </div>
+    );
+};
+
+export default CFXR;
+
